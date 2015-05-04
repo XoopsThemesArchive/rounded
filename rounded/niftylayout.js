d8195491823a2e6b8e17c069115a72f3f6924a9c
mambax7 <mambax7@gmail.com>
2015-05-04 05:49:28 -0400

    Original files

---
+++ rounded/niftylayout.js
@@ -0,0 +1,17 @@
+/*nifty corners layout*/
+
+window.onload=function(){
+Nifty("div#menu a","small transparent top");
+Nifty("ul#intro li","big same-height");
+Nifty("div.date");
+Nifty("div#content,div#side","same-height");
+Nifty("div#block-cl,div#block-cr","same-height");
+Nifty("div.comments div");
+Nifty("div.blockcl");
+Nifty("div.blockcr");
+Nifty("div.blockr");
+Nifty("div.blockcc");
+Nifty("div#block-cc");
+Nifty("div#footer");
+Nifty("div#container","bottom");
+}
